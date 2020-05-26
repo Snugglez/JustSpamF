82f7c06c45a348db90c0af0e29108543fe404d83
Snugglez <32309975+Snugglez@users.noreply.github.com>
2020-05-26 17:31:03 -0400

    nothing matters here

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 module.exports = function justspamf(d) {
-  d.hook('S_DIALOG', '*', (e) => {
+  d.hook('S_DIALOG', 2, (e) => {
     for (let i = 0; i < e.options.length; i++) {e.options[i].style = 43}
     e.type = 1
     return true
