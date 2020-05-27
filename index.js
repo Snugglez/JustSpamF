28095bfd2a856026b1f811165eb7328c61e649a6
Snugglez <32309975+Snugglez@users.noreply.github.com>
2020-05-27 08:40:42 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 module.exports = function justspamf(d) {
-  d.hook('S_DIALOG', 2, (e) => {
-    for (let i = 0; i < e.options.length; i++) {e.options[i].style = 43}
+  d.hook('S_DIALOG', 3, (e) => {
+    for (let i = 0; i < e.buttons.length; i++) { if ([1, 2, 4, 5, 51, 53, 54, 55, 56, 63].includes(e.buttons[i].type)) e.buttons[i].type = 43 }
     e.type = 1
     return true
   })
