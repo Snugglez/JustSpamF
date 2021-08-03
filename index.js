17e31a7058960b54fd7fad5564dcb588f3363101
Snugglez <32309975+Snugglez@users.noreply.github.com>
2021-08-03 10:58:43 -0400

    kill me

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 module.exports = function justspamf(d) {
-  d.hook('S_DIALOG', 3, (e) => {
+  d.hook('S_DIALOG', 4, (e) => {
     if (!e.buttons.length || !d.settings.enabled) return
     for (let i = 0; i < e.buttons.length; i++) { if ([1, 2, 3, 4, 5, 51, 53, 54, 55, 56, 63].includes(e.buttons[i].type)) e.buttons[i].type = 43 }
     e.type = 1
     return true
   })
-  d.command.add('jsf', () => {
-    d.settings.enabled = !d.settings.enabled
-    d.command.message(`justSpamF is now ${d.settings.enabled ? 'enabled' : 'disabled'}.`)
+  d.command.add('jsf', (arg) => {
+    if (!arg) {
+      d.settings.enabled = !d.settings.enabled
+      d.command.message(`${d.settings.enabled ? 'en' : 'dis'}abled`)
+    }
   })
 }
