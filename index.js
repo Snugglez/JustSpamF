3f8ad6d8584a59ca6e8521862fa1b56456b64a23
Snugglez <32309975+Snugglez@users.noreply.github.com>
2021-09-28 19:08:49 -0400

    v110

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 module.exports = function justspamf(d) {
-  d.hook('S_DIALOG', 4, (e) => {
+  d.hook('S_DIALOG', 5, (e) => {
     if (!e.buttons.length || !d.settings.enabled) return
     for (let i = 0; i < e.buttons.length; i++) { if ([1, 2, 3, 4, 5, 51, 53, 54, 55, 56, 63].includes(e.buttons[i].type)) e.buttons[i].type = 43 }
     e.type = 1
